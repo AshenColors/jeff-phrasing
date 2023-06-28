15fb40c5623a56a6697887c264b2e798681d2f3f
AshenColors <3249074+AshenColors@users.noreply.github.com>
2023-06-28 14:24:22 -0400

    exclude some valid cases, make max_examples more reasonable

--- jeff-phrasing_test.py
+++ jeff-phrasing_test.py
@@ -1,15 +1,17 @@
-from hypothesis import given, example, settings, Verbosity
+from hypothesis import given, example, settings, assume
 from hypothesis.strategies import from_regex
 
 import importlib
 jp = importlib.import_module("jeff-phrasing")
 
-@settings(max_examples=1000000, verbosity=Verbosity.verbose)
+@settings(max_examples=10000)
 # Without fullmatch here, garbage before/after the string matching the regex is added
 @given(from_regex(jp.PARTS_MATCHER, fullmatch=True))
 def test_roundtrip(outline):
+    assume(outline)
     try:
         answer = jp.lookup([outline]).strip()
+        assume(answer)
     except:
         pass
     else:
