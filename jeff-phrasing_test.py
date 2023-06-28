483c62992df767111e94b8abe334ae7432efbc9e
AshenColors <3249074+AshenColors@users.noreply.github.com>
2023-06-28 13:25:32 -0400

    add fullmatch to limit tested garbage

--- jeff-phrasing_test.py
+++ jeff-phrasing_test.py
@@ -1,11 +1,12 @@
-from hypothesis import given, example, settings
+from hypothesis import given, example, settings, Verbosity
 from hypothesis.strategies import from_regex
 
 import importlib
 jp = importlib.import_module("jeff-phrasing")
 
-@settings(max_examples=1000000, report_multiple_bugs=True)
-@given(from_regex(jp.PARTS_MATCHER))
+@settings(max_examples=1000000, verbosity=Verbosity.verbose)
+# Without fullmatch here, garbage before/after the string matching the regex is added
+@given(from_regex(jp.PARTS_MATCHER, fullmatch=True))
 def test_roundtrip(outline):
     try:
         answer = jp.lookup([outline]).strip()
